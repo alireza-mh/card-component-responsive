d4f23197ef69b24a17a2aa4f106b84c45fb9ab7a
Alireza <alireza.mh@gmail.com>
2020-02-09 12:49:49 +0330

    Updates

---
+++ precache-manifest.2f4382de428f5d5d4f045e3b18586ff1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1e738af4da70b2ff21",
+    "url": "./commons~main~vendor.2d1e.js"
+  },
+  {
+    "revision": "5459e8e66594f1ac196a5eca4857e465",
+    "url": "./images/boat-image.jpg"
+  },
+  {
+    "revision": "a08354e72a7e140dffaebcc169e536de",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e247ab4a23498eb3bb61",
+    "url": "./main.e247.css"
+  },
+  {
+    "revision": "e247ab4a23498eb3bb61",
+    "url": "./main.e247.js"
+  },
+  {
+    "revision": "15ba8e208b772e07f1bf",
+    "url": "./runtime~main.js"
+  },
+  {
+    "revision": "a6ea2ac06496d95e691f",
+    "url": "./runtime~vendor.js"
+  },
+  {
+    "revision": "05e041797d7f2746ea99",
+    "url": "./vendor.05e0.js"
+  },
+  {
+    "revision": "da8b59eef05b863bf490",
+    "url": "./vendors~main.da8b.js"
+  }
+]);
---
+++ vendor.05e0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{155:function(n,o,p){p(0),n.exports=p(57)}},[[155,3,0]]]);
